3d8e76080de25a0e2b7db0089f2efba5e1276514
Davi Seagull <daviseagull@ibm.com>
2023-06-22 19:55:42 -0300

    feature: add service to controller

--- src/messages/controllers/messages.controller.ts
+++ src/messages/controllers/messages.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from '../dtos/create-message.dto';
+import { MessagesService } from '../services/messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param() id: string) {
-    console.log(id);
+    const message = this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
   }
 }
--- src/messages/repositories/messages.repository.ts
+++ src/messages/repositories/messages.repository.ts
@@ -1,9 +1,28 @@
-import { readFile, writeFile } from "fs";
+import { readFile, writeFile } from 'fs/promises';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
 
-  async findAll() {}
+    return messages[id];
+  }
 
-  async create(message: string) {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    return JSON.parse(contents);
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
